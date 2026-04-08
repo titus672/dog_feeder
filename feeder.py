0d08bad7170566dbf32a0d1e96cee6ad68d453ec
titus672 <titusfreezn@gmail.com>
2026-04-08 13:40:33 -0700

    iteration

--- feeder.py
+++ feeder.py
@@ -9,7 +9,7 @@ class DogFeeder:
         self.status = 0
     # function to test stepper motor, accepts a number of steps and a delay in milliseconds
 
-    def test_stepper(self, steps, delay):
+    def drive_stepper(self, steps, delay):
         if steps <= 0:
             self.status = 1
             print("Error, steps must be a positive integer")
--- main.py
+++ main.py
@@ -15,8 +15,14 @@ async def status():
     return {"message": feeder.status}
 
 
+@app.post("/move_stepper")
+async def move_stepper(steps: int = 0, delay: float = 0.5):
+    feeder.drive_stepper(steps, delay)
+    return {"Message": f"Moved stepper {steps} steps"}
+
+
 def main():
-    feeder.test_stepper(200, 5)
+    feeder.drive_stepper(3200, 1)
 
 
 if __name__ == "__main__":
